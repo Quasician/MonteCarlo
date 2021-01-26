ed9ce8ec9ae3101aa84e2131f40538883c203056
thomasChemmanoor <thomas.chemmanoor@gmail.com>
2021-01-26 12:25:34 -0500

    Finished the rest of the code

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -3,6 +3,8 @@ import math
 import matplotlib.pyplot as plt
 import unicodedata
 
+goodEstimates = []
+
 def throwDarts(cirlceDarts,times):
     throwsX = np.random.rand(times)
     throwsY = np.random.rand(times)
@@ -14,6 +16,8 @@ def throwDarts(cirlceDarts,times):
     checkDarts(throwsX,throwsY, cirlceDarts,trials)
 
 def checkDarts(xPoints, yPoints, circleDarts,trials):
+    goodEstimate = "";
+    threshold = 0.005
     probabilities = []
     i = 1
     for x, y in zip(xPoints, yPoints):
@@ -21,18 +25,59 @@ def checkDarts(xPoints, yPoints, circleDarts,trials):
             circleDarts.append(1)
         else:
             circleDarts.append(0)
-        probability = np.sum(circleDarts) / len(circleDarts)
+        probability = np.cumsum(circleDarts)[-1]/ len(circleDarts)
         probability = probability * 4
+        if math.pi-threshold <= probability <= math.pi+threshold  and not goodEstimate :
+            goodEstimate = [("Trial " + str(i)+ " -> " + "Probability is: " + str(probability)),(i),(probability)]
         probabilities.append(probability)
-        print("Trial " + str(i)+ " -> " + "Probability is: " + str(probability))
+        # print("Trial " + str(i)+ " -> " + "Probability is: " + str(probability))
         i = i+1
-    plt.plot(trials, probabilities)
+
+    # Code for the case where there was no good estimate
+    if not goodEstimate:
+        # Calculate the closest value to pi and return that sample and probability
+        i = 1
+        min = [(10),(0)]
+        for val in probabilities:
+            if(abs(val -math.pi) < abs(min[0] - math.pi)):
+                min = [(val),(i)]
+            i = i+1
+        goodEstimate = [(""), (min[1]), (min[0])]
+
+    # Code for plotting
+    plt.plot(trials, probabilities, label = 'Estimated ' + unicodedata.lookup("GREEK SMALL LETTER PI"))
+    pi = [math.pi]*1000
+    plt.plot(trials, pi, label = unicodedata.lookup("GREEK SMALL LETTER PI"))
     plt.xlabel('# of Monte Carlo Samples')
     plt.ylabel('Estimate of ' + unicodedata.lookup("GREEK SMALL LETTER PI"))
     plt.title('Estimate of ' + unicodedata.lookup("GREEK SMALL LETTER PI")+ ' vs. # of Monte Carlo Samples')
+    plt.legend()
     plt.show()
 
+    global goodEstimates
+    goodEstimates.append(goodEstimate)
+
 if __name__ == '__main__':
-    print("test")
+    # Code for creating the plot for one test
     circleDarts = []
-    throwDarts(circleDarts,1000)
+    throwDarts(circleDarts, 1000)
+
+
+    # Code for creating the plot for 4 addional tests
+    for x in range(4):
+        circleDarts = []
+        throwDarts(circleDarts,1000)
+
+    # Code printing out how many samples are needed for a good estimate individually (also prints out the sample estimate for the first test)
+    for tuple in goodEstimates:
+        if tuple[0]:
+            print(tuple[0])
+        else:
+            print("No trial was close enough for a good estimate, however, this was the closest: " + ("Trial " + str(tuple[1])+ " -> " + "Probability is: " + str(tuple[2])))
+
+    # Code for figuring out how many samples are needed for a good estimate when all 5 experiments are considered in aggregate
+    sum = 0
+    for tuple in goodEstimates:
+        sum = sum + tuple[1]
+    print(str(sum/len(goodEstimates)) + " Monte Carlo samples are needed arrive at a good estimate of " + unicodedata.lookup("GREEK SMALL LETTER PI"))
+
